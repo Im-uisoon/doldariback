b7b0d53fd55fe972b86da6e9a2f952a920356332
Im-uisoon <dla5420@naver.com>
2025-09-26 05:36:59 +0000

    ss

--- doldariserver/src/main/java/com/project/doldariserver/config/WebConfig.java
+++ doldariserver/src/main/java/com/project/doldariserver/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.project.doldariserver.config;
 
 import jakarta.servlet.SessionCookieConfig;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,16 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(28800);
     }
 
-    // 세션 쿠키 설정
     @Bean
-    public ServletContextInitializer servletContextInitializer() {
-        return servletContext -> {
-            SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
-            sessionCookieConfig.setHttpOnly(true);          // JS 접근 불가
-            sessionCookieConfig.setSecure(true);            // HTTPS 환경에서만
-            sessionCookieConfig.setName("JSESSIONID");      // 기본 세션 이름
-            sessionCookieConfig.setPath("/");               // 전체 경로에서 쿠키 전달
-            sessionCookieConfig.setMaxAge(-1);              // 브라우저 종료 시까지
-        };
+    public CookieSameSiteSupplier cookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
     }
 }
